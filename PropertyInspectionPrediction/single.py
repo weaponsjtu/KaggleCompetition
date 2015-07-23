4718dbfbfa22395064704897ed2b4a127503b7c3
nightcrawler <javawebzwp@gmail.com>
2015-07-23 13:40:48 +0800

    cross validation with Xgboost

--- PropertyInspectionPrediction/single.py
+++ PropertyInspectionPrediction/single.py
@@ -9,6 +9,70 @@ from hyperopt import fmin, tpe, hp, Trials, STATUS_OK
 
 from utils import *
 
+def feature_engineer():
+    train = pd.read_csv('data/train.csv', header=0)
+    test = pd.read_csv('data/test.csv', header=0)
+
+    labels = train.Hazard
+    train.drop('Hazard', axis=1, inplace=True)
+
+    idx = test.Id
+
+    train = np.array(train)
+    test = np.array(test)
+
+    # label encode the categorical variables
+    for i in range(train.shape[1]):
+        lbl = preprocessing.LabelEncoder()
+        lbl.fit(list(train[:,i]) + list(test[:,i]))
+        train[:,i] = lbl.transform(train[:,i])
+        test[:,i] = lbl.transform(test[:,i])
+
+    return train, labels, test, idx
+
+
+def train_model(train, labels, test, idx):
+    params = {}
+    params["objective"] = "reg:linear"
+    params["eta"] = 0.005
+    params["min_child_weight"] = 5
+    params["subsample"] = 0.75
+    params["colsample_bytree"] = 0.85
+    params["scale_pos_weight"] = 1.0
+    params["silent"] = 1
+    params["max_depth"] = 8
+
+
+    num_rounds = 10000
+    xgtest = xgb.DMatrix(test)
+
+    kfold = 10
+    kiter = 2
+    train_subsets, labels_subsets = cv_split(train, labels, kfold, kiter)
+
+    best_score = 0
+    best_model = None
+    for i in range(kiter):
+        gini_sum = 0
+        for f in range(kfold):
+            #create a train and validation dmatrices
+            xgtrain = xgb.DMatrix(train_subsets[i][f][0], label=labels_subsets[i][f][0])
+            xgval = xgb.DMatrix(train_subsets[i][f][1], label=labels_subsets[i][f][1])
+
+            #train using early stopping and predict
+            watchlist = [(xgtrain, "train"),(xgval, "val")]
+            model = xgb.train(params, xgtrain, num_rounds, watchlist, early_stopping_rounds=100)
+
+            pred_val = model.predict(xgval)
+            gini_f = Gini(labels_subsets[i][f][1], pred_val)
+            gini_sum += gini_f
+        print "Iter %d, Gini Mean is %f" %(i, gini_sum/kfold)
+
+    write_submission(idx, pred, 'single_best.csv')
+    return pred
+
+
+
 
 def hyperopt_obj(param, train, label_t, val, label_v):
     gini_score = train_one_model(param, train, label_t, val, label_v)
